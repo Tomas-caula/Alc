7b5108ebc6825dec621be266f32d7393a3856215
Tomas-caula <70955134+Tomas-caula@users.noreply.github.com>
2025-11-10 23:47:46 -0300

    Update tp.py

--- tp.py
+++ tp.py
@@ -1,29 +1,30 @@
 from ALC import *
 import numpy as np
-import os 
 
 
 def cargarCarpeta(carpeta):
-    embeddingsGatos = np.load( os.path.join(carpeta, 'train', 'cats', 'efficientnet_b3_embeddings.npy'))
-    embeddingsPerros = np.load( os.path.join(carpeta, 'train', 'dogs', 'efficientnet_b3_embeddings.npy'))
+    embeddingsGatos = np.load( carpeta + 'train/cats/efficientnet_b3_embeddings.npy')
+    embeddingsPerros = np.load( carpeta + 'train/dogs/efficientnet_b3_embeddings.npy')
 
-    embeddingGatosVal = np.load( os.path.join(carpeta, 'val', 'cats', 'efficientnet_b3_embeddings.npy'))
-    embeddingPerrosVal = np.load( os.path.join(carpeta, 'val', 'dogs', 'efficientnet_b3_embeddings.npy'))
+    embeddingGatosVal = np.load( carpeta + 'val/cats/efficientnet_b3_embeddings.npy')
+    embeddingPerrosVal = np.load(carpeta + 'val/dogs/efficientnet_b3_embeddings.npy')
 
     Xv = np.concatenate((embeddingGatosVal, embeddingPerrosVal))
     Yv = np.concatenate((
-        np.zeros((embeddingGatosVal.shape[0], 1)),
-        np.ones((embeddingPerrosVal.shape[0], 1))
+        np.array([np.array([1, 0])] * embeddingGatosVal.shape[0]), #Gatos es (1,0)
+        np.array([np.array([0, 1])] * embeddingPerrosVal.shape[0])
     ))
 
     Xt = np.concatenate((embeddingsGatos, embeddingsPerros))
 
     Yt = np.concatenate((
-        np.zeros((embeddingsGatos.shape[0], 1)),
-        np.ones((embeddingsPerros.shape[0], 1))
+        np.array([np.array([1, 0])] * embeddingsGatos.shape[0]),
+        np.array([np.array([0, 1])] * embeddingsPerros.shape[0])
     ))
     return Xt, Yt, Xv, Yv
     
-Xt, Yt = cargarCarpeta('./cats_and_dogs/')
-print(Xt)
-print(Yt)
+Xt, Yt, Xv, Yv = cargarCarpeta('./cats_and_dogs/')
+
+
+def pinvEcuacionesNormales(X, L, Y):
+    
